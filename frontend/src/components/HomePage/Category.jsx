477fb4e9554334d5240e33eaf6979cfb4afc2959
German <g.oroceo@mylogoinc.com>
2024-02-08 07:43:11 +0800

    feat: add toggle option to display and hide all categories

--- frontend/src/components/HomePage/Category.jsx
+++ frontend/src/components/HomePage/Category.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+//Dummy
 const urlImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsJxCQC_E5Li1l5SsPu81QQZLsZsxLh-aCiBRU7aNgsThXw-VG6gFT5lvog0I4BnBDKoo&usqp=CAU";
-const jobCategories = ['Plumber', 'Carpenter', 'Event Staff', 'Aircon Technician', 'Lot Surveyor', 'Lot Surveyor', 'Lot Surveyor', 'See All Categories'];
+const jobCategories = ['Accountant / Bookkeeping', 'Acupuncture', 'Auto Mechanic', 'Art Services', 'Caregiver', 'Carpenter', 'CCTV Maintenance', 'Driver', 'Electrical Services', 'Electronics Technician', 'Entertainment', 'I.T. Services', 'Graphic Artist', 'Home Cleaning', 'Laundry', 'Medical Services', 'Pest Control', 'Physical Therapy', 'Plumber', 'Rent A Car', 'Salon', 'Security', 'Spa & Massage', 'Tattoo Services', 'Travel and Tours', 'Tutor', 'Technician', 'Photography / Videography', 'Welder'];
 
 export default function Category() {
+    const [showAll, setShowAll] = useState(false);
+
+    const toggleShowAll = () => {
+        setShowAll(!showAll);
+    };
+
+    const allCategories = showAll ? jobCategories : jobCategories.slice(0, 6);
+
     return (
         <div className="w-full flex justify-center">
             <div className="">
                 <div className="flex flex-wrap w-fit">
-                    {jobCategories.map((category, index) => (
+                    {allCategories.map((category, index) => (
                         <div key={index} className='m-2 p-4 text-center justify-center max-w-28'>
                             <img src={urlImage} className="rounded-xl object-cover" />
                             <div className="my-2 m-auto text-wrap">
@@ -16,6 +25,18 @@ export default function Category() {
                             </div>
                         </div>
                     ))}
+                    {!showAll && (
+                        <div onClick={toggleShowAll} className='m-2 p-4 text-center justify-center max-w-28 cursor-pointer'>
+                            <img src='https://cdn-icons-png.flaticon.com/512/5579/5579229.png' className="rounded-xl object-cover" />
+                            <button className="text-gray-600 hover:text-gray-900 mt-2 self-end">See All Categories</button>
+                        </div>
+                    )}
+                    {showAll && (
+                        <div onClick={toggleShowAll} className='m-2 p-4 text-center justify-center max-w-28 cursor-pointer'>
+                            <img src='https://cdn-icons-png.flaticon.com/256/166/166425.png' className="rounded-xl object-cover" />
+                            <button className="text-gray-600 hover:text-gray-900 mt-2 self-end">See less</button>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
